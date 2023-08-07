4d5163f091a337026a14c1b665de19f716fbc894
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-08-08 01:17:21 +0400

    added connected check

--- src/components/StakingBox.jsx
+++ src/components/StakingBox.jsx
@@ -246,16 +246,18 @@ function StakingBox() {
     <div className="bg-feature-card-border p-2 rounded-lg shadow-[0_0_1rem_rgba(0,0,0,1)] ">
       <div className="border-2 border-main-green-shade-40 rounded-xl pt-6 pb-10 bg-green-radial transition-all duration-300">
         <header className="px-4 sm:px-6 mb-7">
-          <h1 className="text-center text-lg sm:text-xl xl:text-2xl mb-3 font-black">
+          <h1 className="text-center text-lg sm:text-xl xl:text-2xl  font-black">
             FLUID STAKING
           </h1>
-          <p className="text-center font-medium mb-2 text-sm">
-            {/* Total $DLANCE in Fluid Staking 476,852,255.89 */}
-            Total $DLANCE in Fluid Staking{" "}
-            {isTotalStakedFetching
-              ? "Fetching..."
-              : Number(formatEther(totalStaked.toString(), "wei")).toFixed(2)}
-          </p>
+          {isConnected ? (
+            <p className="text-center font-medium mb-2 text-sm mt-3">
+              {/* Total $DLANCE in Fluid Staking 476,852,255.89 */}
+              Total $DLANCE in Fluid Staking{" "}
+              {isTotalStakedFetching
+                ? "Fetching..."
+                : Number(formatEther(totalStaked, "wei").toString()).toFixed(2)}
+            </p>
+          ) : null}
           <p className="text-center font-bold text-sm xl:text-base">
             APY: {APY_FLUID_STAKING}%
           </p>
